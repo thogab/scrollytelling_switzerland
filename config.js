ebbee47db896d75c9756bb56d4b522c887798bc4
thogab <107146945+thogab@users.noreply.github.com>
2022-09-10 19:25:28 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,235 @@
+var config = {
+    style: 'mapbox://styles/thogab/cl7uu65lq001s15n0v46gg5wx',
+    accessToken: 'pk.eyJ1IjoidGhvZ2FiIiwiYSI6ImNsN3V1eTYxczA1a3U0MG96bXdiemhvMXUifQ.Y6LhDT6ISM2-kheUrhn6dA',
+    showMarkers: false,
+    theme: 'light',
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'effretikon_01',
+            alignment: 'right',
+            title: 'Effretikon',
+            image: '',
+            description: '',
+            location: {
+                center: [8.682824, 47.419917],
+                zoom: 16.87,
+                pitch: 75,
+                bearing: 67.2
+            },
+        },
+        {
+            id: 'arthgoldau_01',
+            alignment: 'right',
+            title: 'Arth-Goldau',
+            image: '',
+            description: '',
+            location: {
+                center: [8.5414, 47.05538],
+                zoom: 14.90,
+                pitch: 82,
+                bearing: -60
+            },
+          
+        },
+        {
+            id: 'waedenswil_01',
+            alignment: 'right',
+            title: 'Wädenswil',
+            image: '',
+            description: '',
+            location: {
+                center: [8.66459, 47.24821],
+                zoom: 13.76,
+                pitch: 82,
+                bearing: -36
+            },
+           
+        },
+        {
+            id: 'zürich_01',
+            alignment: 'right',
+            title: 'Zürich',
+            image: '',
+            description: '',
+            location: {
+                center: [8.493129, 47.394672],
+                zoom: 15.86,
+                pitch: 75.00,
+                bearing: -50.4
+            },
+        
+        },
+        {
+            id: 'oberwinterthur_01',
+            alignment: 'right',
+            title: 'Oberwinterthur',
+            image: '',
+            description: '',
+            location: {
+                center: [8.765059, 47.511305],
+                zoom: 17.69,
+                pitch: 73,
+                bearing: 52.8
+            },
+       
+        },
+        {
+            id: 'basel_01',
+            alignment: 'right',
+            title: 'Basel/Muttenz',
+            image: '',
+            description: '',
+            location: {
+                center: [7.664659, 47.533198],
+                zoom: 15.63,
+                pitch: 79,
+                bearing: -36
+            },
+        },
+        {
+            id: 'walenstadt_01',
+            alignment: 'right',
+            title: 'Bommersteintunnel',
+            image: '',
+            description: '',
+            location: {
+                center: [9.26105, 47.12741],
+                zoom: 13.42,
+                pitch: 78.00,
+                bearing: -99.2
+            },
+        
+        },
+        {
+            id: 'landquart_01',
+            alignment: 'right',
+            title: 'Landquart',
+            image: '',
+            description: '',
+            location: {
+                center: [9.53682, 46.95998],
+                zoom: 13.88,
+                pitch: 76.00,
+                bearing: -163.2
+            },
+        
+        },
+        {
+            id: 'embd_01',
+            alignment: 'right',
+            title: 'Embd',
+            image: '',
+            description: '',
+            location: {
+                center: [7.79491, 46.22189],
+                zoom: 13.16,
+                pitch: 76.00,
+                bearing: -138.4
+            },
+        
+        },
+        {
+            id: 'gliesergrund_01',
+            alignment: 'right',
+            title: 'Gliesergrund',
+            image: '',
+            description: '',
+            location: {
+                center: [7.98011, 46.3184],
+                zoom: 15.03,
+                pitch: 81.00,
+                bearing: 49.7
+            },
+        
+        },
+        {
+            id: 'litzirueti_01',
+            alignment: 'right',
+            title: 'Litzirüti',
+            image: '',
+            description: '',
+            location: {
+                center: [9.69677, 46.81065],
+                zoom: 14.41,
+                pitch: 79.00,
+                bearing: -153.3
+            },
+        
+        },
+        {
+            id: 'untersax_01',
+            alignment: 'right',
+            title: 'Untersax',
+            image: '',
+            description: '',
+            location: {
+                center: [9.59833, 46.83539],
+                zoom: 13.75,
+                pitch: 83,
+                bearing: 68
+            },
+        
+        },
+        {
+            id: 'klosters-01',
+            alignment: 'right',
+            title: 'Klosters',
+            image: '',
+            description: '',
+            location: {
+                center: [9.89442, 46.85683],
+                zoom: 14.15,
+                pitch: 84,
+                bearing: 122.2
+            },
+        
+        },
+        {
+            id: 'tavanasas_01',
+            alignment: 'right',
+            title: 'Tavanasas',
+            image: '',
+            description: '',
+            location: {
+                center: [9.06291, 46.7638],
+                zoom: 14.48,
+                pitch: 82,
+                bearing: -81.3
+            },
+        
+        },
+        {
+            id: 'vilmergen_01',
+            alignment: 'right',
+            title: 'Vilmergen',
+            image: '',
+            description: '',
+            location: {
+                center: [8.246282, 47.356888],
+                zoom: 16.76,
+                pitch: 76,
+                bearing: 174.5
+            },
+        
+        },
+        {
+            id: 'huentnwang_01',
+            alignment: 'right',
+            title: 'Hüntnwang',
+            image: '',
+            description: '',
+            location: {
+                center: [8.506773, 47.589159],
+                zoom: 16.19,
+                pitch: 75,
+                bearing: -49.6
+            },
+        
+        },
+
+    ]
+};
